cedda2eb5691703559adab540ff3b9646b64bc72
Jonath-679 <jonathan.10.craft@gmail.com>
2026-08-07 13:07:01 -0600

    refactor(hyprland-monitors): replace monitorAny with monitorExterno

--- hypr/modules/monitors.lua
+++ hypr/modules/monitors.lua
@@ -3,7 +3,7 @@
 --- Monitors
 local monitorLaptop = "eDP-1"
 local monitorAcer = "desc:Acer Technologies KB242Y G0 95380BD865W01"
-local monitorAny = "HDMI-A-1" -- this includes monitorAcer
+local monitorExterno = "HDMI-A-1" -- this includes monitorAcer
 
 --- Config
 hl.monitor({output=monitorLaptop, mode="1920x1080@60", position="0x0", scale="1.0"})
@@ -16,9 +16,9 @@ hl.workspace_rule({workspace="2", monitor=monitorLaptop})
 hl.workspace_rule({workspace="3", monitor=monitorLaptop})
 hl.workspace_rule({workspace="4", monitor=monitorLaptop})
 hl.workspace_rule({workspace="5", monitor=monitorLaptop})
-hl.workspace_rule({workspace="6", monitor=monitorAny})
-hl.workspace_rule({workspace="7", monitor=monitorAny})
-hl.workspace_rule({workspace="8", monitor=monitorAny})
-hl.workspace_rule({workspace="9", monitor=monitorAny})
-hl.workspace_rule({workspace="10", monitor=monitorAny})
+hl.workspace_rule({workspace="6", monitor=monitorExterno})
+hl.workspace_rule({workspace="7", monitor=monitorExterno})
+hl.workspace_rule({workspace="8", monitor=monitorExterno})
+hl.workspace_rule({workspace="9", monitor=monitorExterno})
+hl.workspace_rule({workspace="10", monitor=monitorExterno})
 
