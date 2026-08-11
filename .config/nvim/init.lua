ec51b066efe3477dd626ddc47bf98f8f6639e4fe
Jonath-679 <jonathan.10.craft@gmail.com>
2026-08-11 13:05:48 -0600

    feat(nvim-lazy.nvim) building my editor
    modularizacion y pequeños ajustes
    
    add catppuccin theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,4 @@
-vim.opt.number = true
-vim.opt.relativenumber = false
-vim.opt.cursorline = true
-vim.opt.wrap = false
-vim.opt.expandtab = false
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.clipboard = "unnamedplus"
-vim.opt.swapfile = false
+require("config.lazy")
+
+require("config/keymaps")
+require("config/options")
