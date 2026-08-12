aa1f2e4ba2ab87bb0b87e55a0c0a43f31a12b0d0
Jonath-679 <jonathan.10.craft@gmail.com>
2026-08-11 19:26:36 -0600

    feat(nvim-lazy.nvim) add snacks.terminal plugin

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -7,7 +7,7 @@ return {
 	opts = {
 		-- bigfile = { enabled = true },
 		-- dashboard = { enabled = true },
-		explorer = {enabled=true},
+		explorer = { enabled = true },
 		-- indent = { enabled = true },
 		-- input = { enabled = true },
 		picker = {enabled=true, sources={explorer={layout={cycle=false}}}},
@@ -17,6 +17,7 @@ return {
 		-- scroll = { enabled = true },
 		-- statuscolumn = { enabled = true },
 		-- words = { enabled = true },
+		terminal = { enabled = true }
 	},
 	keys = {
 		-- Top Pickers & Explorer
@@ -26,6 +27,31 @@ return {
 		{ "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
 		{ "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
 		{ "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+		
+		-- Terminal
+		{ "<leader>t",
+			function()
+				local buf_dir = vim.fn.expand("%:p:h")
+				local valid_cwd = (vim.fn.isdirectory(buf_dir) == 1) and buf_dir or vim.fn.getcwd()
+				Snacks.terminal.toggle(nil, {
+					id = "terminal_principal",
+					cwd = valid_cwd,
+					win = {
+						position = "bottom",
+						height = 0.3,
+						wo = {
+							statusline = " ", 
+							winbar = "",      
+						}
+					},
+					start_insert = true,
+					auto_insert = true,
+					auto_close = false,
+				})
+			end,
+			desc = "Terminal toggle abajo (split)",
+			mode = { "n", "t" },
+	   }
 	}
 }
 
