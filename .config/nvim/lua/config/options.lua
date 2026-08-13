800d00e1de7f1df5a1d494c029f86af89f666846
Jonath-679 <jonathan.10.craft@gmail.com>
2026-08-13 10:03:59 -0600

    feat(nvim-lazy.nvim): update options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,13 +1,47 @@
-vim.opt.number = true
-vim.opt.relativenumber = false
+-- Appearance 
 vim.opt.cursorline = true
+vim.opt.number = true
 vim.opt.wrap = false
-vim.opt.expandtab = false
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes:1"
+vim.opt.pumheight = 10 -- max completion popup height
+-- vim.opt.colorcolumn = "150" -- visual line-length guide
+-- vim.opt.scrolloff = 5
+-- vim.opt.sidescrolloff = 5
+
+-- Tabs & indentation 
+vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+
+-- clipboard and temporary files 
 vim.opt.clipboard = "unnamedplus"
 vim.opt.swapfile = false
 
-vim.opt.termguicolors = true
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
+-- Splits
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Highlight the text briefly when copying (yanking) it. 
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- Extra
+-- vim.opt.scrolloff = 5
+-- vim.opt.sidescrolloff = 5 
+
+-- Plugins
+vim.g.mapleader = " "
 vim.diagnostic.config({virtual_text=true})
+
