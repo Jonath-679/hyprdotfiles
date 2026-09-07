8067e05eef2c84043542da696322dd6036a2a8f8
Jonath-679 <jonathan.10.craft@gmail.com>
2026-09-07 11:23:14 -0600

    chore(hyprland): update hymission.lua
    se mantuvo la misma configuracion que antes, solo se removieron/agregaron configuraciones por parte de hymission

--- .config/hypr/plugins/hymission.lua
+++ .config/hypr/plugins/hymission.lua
@@ -42,14 +42,17 @@ hl.config({
             multi_workspace_sort_recent_first = 0, --1
             only_active_workspace = 0,
             only_active_monitor = 0,
-            show_special = 0, --1
+            show_special = 0,
             workspace_change_keeps_overview = 1,
 
             -- Behavior: hover and selection
-            expand_selected_window = 1,
+            selected_expand_scale = 1.06, --1.18
             hover_expand_scale = 1.06, --1.18
             overview_focus_follows_mouse = 1,
             show_focus_indicator = 0,
+            grouped_windows_policy = "expanded",
+            grouped_windows_collapsed_labels = 1,
+            grouped_windows_collapsed_scroll = 1,
 
             -- Animation: hover relayout
             hover_relayout_animation = "",
@@ -73,8 +76,11 @@ hl.config({
             workspace_strip_empty_mode = "existing",
             workspace_strip_thickness = 160,
             workspace_strip_gap = 24,
+            workspace_strip_force_show = 0,
+            workspace_strip_refresh_ms = 500,
             hide_bar_when_strip = 1,
             hide_hyprbars_during_overview = 0,
+            hide_hyprglass_during_overview = 1,
             bar_single_mission_control = 0,
             hide_bar_animation = 1,
             hide_bar_animation_blur = 1,
@@ -83,7 +89,8 @@ hl.config({
             hide_bar_animation_alpha_end = 0,
 
             -- Label picking and window controls
-            pick_labels_enabled = 0,
+            pick_labels_enabled = 1, --0
+            pick_labels_show = 1,
             pick_labels_mode = "sequential",
             pick_labels_direct_activate = 0,
             window_decoration_enabled = 1,
@@ -94,7 +101,6 @@ hl.config({
             -- Appearance and color customization
             backdrop_blur = 0,
             backdrop_color = "rgba(000000AB)", --rgba(00000000)
-            focus_hover_color = "rgba(f2f7ff8c)",
             focus_selected_color = "rgba(3dc7fff2)",
             focus_hover_thickness = 2,
             focus_selected_thickness = 4,
