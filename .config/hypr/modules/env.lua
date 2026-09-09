28d5348d18947ab6443a0ca1e135fff2d2c9f51b
Jonath-679 <jonathan.10.craft@gmail.com>
2026-09-08 23:47:46 -0600

    chore(hyprland): minor changes

--- .config/hypr/modules/env.lua
+++ .config/hypr/modules/env.lua
@@ -1,11 +1,18 @@
 --- ~/.config/hypr/modules/env.lua
 
+--- General
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
 hl.env("XDG_SESSION_DESKTOP", "Hyprland")
 
+--- QT
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 
+--- Cursor
+hl.env("XCURSOR_THEME", "Sweet-cursors")
+hl.env("XCURSOR_SIZE", "24")
+
+--- Electron
 hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 
--- .config/hypr/modules/gestures.lua
+++ .config/hypr/modules/gestures.lua
@@ -1,7 +1,7 @@
 --- ~/.config/hypr/modules/gestures.lua
 
-hl.config({gestures={workspace_swipe_distance=300, workspace_swipe_invert=false}})
+hl.config({ gestures = { workspace_swipe_distance = 300, workspace_swipe_invert = false }})
 
-hl.gesture({fingers=4, direction="horizontal", action="workspace"})
-hl.gesture({fingers=4, direction="vertical", action="workspace"})
+hl.gesture({ fingers = 4, direction = "horizontal", action = "workspace" })
+hl.gesture({ fingers = 4, direction = "vertical", action = "workspace" })
 
